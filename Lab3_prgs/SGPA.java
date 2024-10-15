d1eb5468d4800d44ff1f9a83e96a5aca90fc4e33
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-10-15 13:03:30 +0530

    Update SGPA.java

--- Lab3_prgs/SGPA.java
+++ Lab3_prgs/SGPA.java
@@ -57,6 +57,7 @@ class Sgpa
 
 
 /* ARRAY OF OBJECTS (NOT WORKING)
+
 import java.util.Scanner;
 
 class Student 
@@ -97,41 +98,41 @@ class Student
 		cgpa=(sgpa1+sgpa2)/2;
 		return cgpa;
 	}
+	void input()
+	{
+	Scanner sc=new Scanner(System.in);
+        System.out.println("Now enter subject credits for semester:");
+        int i;
+        for (i = 0; i < 5; i++) 
+        {
+            credits[i] = sc.nextInt();
+        }
+        System.out.println("Now enter subject marks for semester:");
+        for (i = 0; i < 5; i++) 
+        {
+            marks[i] = sc.nextInt();
+        }
+	}
 
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+	Scanner sc1 = new Scanner(System.in);
 	System.out.println("Enter number of students: ");
-	int n=sc.nextInt();
+	int n=sc1.nextInt();
 	Student obj[]=new Student[n];
 	int k;
 	for(k=0;k<n;k++)
 	{
-        System.out.println("Enter name and then USN:");
-        obj[k].name = sc.nextLine();
-        obj[k].usn = sc.nextLine();
-        System.out.println("Now enter subject credits for Sem 1:");
-        int i;
-        for (i = 0; i < 5; i++) 
-        {
-            obj[k].credits[i] = sc.nextInt();
-        }
-        System.out.println("Now enter subject marks for Sem 1:");
-        for (i = 0; i < 5; i++) 
-        {
-            obj[k].marks[i] = sc.nextInt();
-        }
+	obj[k]=new Student();
+	System.out.println("Enter Student name: ");
+        name = sc1.nextLine();
+	System.out.println("Enter Student USN: ");
+        usn = sc1.nextLine();
+	obj[k].input();
+	System.out.println("Semester 1");
         double result = obj[k].calculate(obj[k].marks, obj[k].credits);
         System.out.println("1st Semester SGPA for " + obj[k].name + " (" + obj[k].usn + ") is: " + result);
-	System.out.println("Now enter subject credits for Sem 2:");
-	for (i = 0; i < 5; i++) 
-        {
-            obj[k].credits[i] = sc.nextInt();
-        }
-        System.out.println("Now enter subject marks for Sem 1:");
-        for (i = 0; i < 5; i++) 
-        {
-            obj[k].marks[i] = sc.nextInt();
-        }
+	System.out.println("Semester 2");
+	obj[k].input();
         double result2 = obj[k].calculate(obj[k].marks, obj[k].credits);
         System.out.println("2nt Semester SGPA for " + obj[k].name + " (" + obj[k].usn + ") is: " + result2);
 	System.out.println("CGPA for 1st year is : "+obj[k].calcgpa(result,result2));
