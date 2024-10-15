8b90158fcbb97ce0d18592b426c3b54ca9b6e06d
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-10-15 13:04:20 +0530

    Create q2.java

---
+++ lab4_prgs/q2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.*;
+class Book
+{
+String name;
+String author;
+int price;
+int num_pages;
+Book(String n, String a, )
+{
+name="test";
+author="testname";
+price=500;
+num_pages=200;
