a15e73f36575a0a62c26c334a3db6e929b6b1c8d
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-10-23 20:56:15 +0530

    Update inner class demo.java

--- lab5_prgs/inner class demo.java
+++ lab5_prgs/inner class demo.java
@@ -1,16 +1,37 @@
-import java.util.*;
-class innerDemo
-{
-int o=10;
-class innerone
-{
-int a=5;
+
+class OuterClass {
+    private String outerField = "Outer class field";
+
+    class InnerClass {
+        void displayInner() {
+            System.out.println("This is the Inner class.");
+            System.out.println("Accessing Outer class field from Inner class: " + outerField);
+        }
+    }
+    static class StaticNestedClass {
+        void displayStaticNested() {
+            System.out.println("This is the Static Nested class.");
+            System.out.println("Static nested class cannot access non-static fields directly.");
+        }
+    }
+    void methodWithLocalClass() {
+        class LocalInnerClass {
+            void displayLocalInner() {
+                System.out.println("This is the Local Inner class inside a method.");
+                System.out.println("Accessing Outer class field from Local Inner class: " + outerField);
+            }
+        }
+        LocalInnerClass localInner = new LocalInnerClass();
+        localInner.displayLocalInner();
+    }
 }
-public static void main(String args[])
-{
-innerDemo obj=new innerDemo();
-innerone ob=obj.new innerone();
-System.out.println(obj.o);
-System.out.println(ob.a);
-}
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        OuterClass outer = new OuterClass();
+        OuterClass.InnerClass inner = outer.new InnerClass(); 
+        inner.displayInner();
+        OuterClass.StaticNestedClass staticNested = new OuterClass.StaticNestedClass();
+        staticNested.displayStaticNested();
+        outer.methodWithLocalClass();
+    }
 }
