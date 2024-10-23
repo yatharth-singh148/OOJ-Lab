0f838f65a1719ad6782cba5ae8deb5a29e1a85dd
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-10-23 19:57:10 +0530

    Update Banking.java

--- lab5_prgs/Banking.java
+++ lab5_prgs/Banking.java
@@ -13,6 +13,7 @@ return sum;
 double Withdraw(double with, double am)
 {
 double sub = am-with;
+return sub;
 }
 public static void main(String args[])
 {
@@ -20,14 +21,21 @@ Banking c[]=new Banking[15];
 Banking obj=new Banking();
 Scanner sc = new Scanner(System.in);
 double dep,with;
+System.out.println("Enter number of customers:");
+int num=sc.nextInt();
 int ch,n;
-System.out.println("Enter 15 bank balances of customers: ");
-for(int i=0;i<=14;i++)
+n=0;
+ch=0;
+System.out.println("Enter "+num+" bank balances of customers respectively: ");
+for(int i=0;i<num;i++)
 {
+c[i]=new Banking();
 c[i].balance=sc.nextDouble();
 }
-/*System.out.println("----------Bank oprations------------");
-System.out.println("1 -> Deposit \n2 -> Withdraw \n3 -> Display Balance\n");
+while(ch!=4)
+{
+System.out.println("----------Bank oprations------------");
+System.out.println("1 -> Deposit \n2 -> Withdraw \n3 -> Display Balance\n4 -> Exit");
 ch=sc.nextInt();
 if(ch==1)
 {
@@ -35,10 +43,19 @@ System.out.println("Enter costumer number for whom to deposit: ");
 n=sc.nextInt();
 System.out.println("Enter amount to deposit: ");
 dep=sc.nextDouble();
+if(dep>0)
+{
 c[n-1].balance= obj.Deposit(dep, c[n-1].balance);
+System.out.println("Amount deposited! New balance: INR "+c[n-1].balance);
+}
+else{
+    System.out.println("Enter an amount greater than 0!");
+}
 }
 if(ch==2)
 {
+System.out.println("Enter costumer number for whom to withdraw: ");
+n=sc.nextInt();
 System.out.println("Enter amount to withdraw: ");
 with=sc.nextDouble();
 if(with>c[n-1].balance)
@@ -48,4 +65,22 @@ System.out.println("Try again and enter a value smaller than the balance INR "+c
 else
 {
 c[n-1].balance=obj.Withdraw(with, c[n-1].balance);
-//complete the prg
+System.out.println("Amount withdrawn! New balance: INR "+c[n-1].balance);
+}
+}
+if(ch==3)
+{
+    System.out.println("Enter customer number to view balance of:");
+    n=sc.nextInt();
+    if(n<=num){
+        System.out.println("INR "+c[n-1].balance);
+    }
+    else{
+        System.out.println("Enter a number ID less than "+num+".");
+    }
+}
+System.out.println("Enter 4 to exit, any other number to repeat.\n");
+ch=sc.nextInt();
+}
+}
+}
