e84901b6d40149926e9fd1fce1a2d61ce193412f
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-11-12 11:48:18 +0530

    Create CharacterArray.java

---
+++ lab7_prgs/CharacterArray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class CharArray
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+int l;
+String str;
+System.out.println("Enter a string: ");
+str=sc.nextLine();
+l=str.length();
+char[] c=new char[l];
+c=str.toCharArray();
+System.out.println("Length: "+l+"\nCharacter array: ");
+for(int i=0;i<l;i++)
+{
+System.out.println(c[i]);
+}
+}
+}
