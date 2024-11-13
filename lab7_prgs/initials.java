530e3024fe31848415b754e77ef3e5bb19046ab8
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-11-13 23:02:22 +0530

    Update initials.java

--- lab7_prgs/initials.java
+++ lab7_prgs/initials.java
@@ -1,23 +1,22 @@
 //q9 lab 7
 import java.util.*;
-class InitialSplit
+class InitialSplit 
 {
-public static void main(String args[])
-{
-Scanner sc = new Scanner(System.in);
-String str;
-String ini;
-System.out.println("Enter name: ");
-str=sc.nextLine();
-char[] c=new char[str.length()];
-c=str.toCharArray();
-ini=""+Character.toString(c[0]);
-System.out.print(c[0]);
-int i=1;
-for(i=1;i<str.length();i++)
-{
-//ini=ini+"."+Character.toString(c[i+1]);
-System.out.print(c[i+1]);
-}
-}
+    public static void main(String args[]) 
+  {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String str = sc.nextLine();
+        String[] words = str.split(" ");
+        String ini = "";
+        for (String word : words)
+        {
+            ini += word.charAt(0) + ".";
+        }
+        if (ini.length() > 0) 
+        {
+            ini = ini.substring(0, ini.length() - 1);
+        }
+        System.out.println(ini);
+    }
 }
