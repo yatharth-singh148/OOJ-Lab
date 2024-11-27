43f79fab55dcfc06d878fc8e13138a1397ca2c55
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-11-27 22:47:10 +0530

    Create two_interfaces.java

---
+++ lab9_prgs/two_interfaces.java
@@ -0,0 +1,15 @@
+//SIMPLE QUEE
+public interface SimpleQueue {
+    void enqueue(int value);
+    int dequeue();
+    boolean isEmpty();
+    int display();
+}
+//STACK
+public interface SimpleStack {
+    void push(int value);
+    int pop();
+    boolean isEmpty();
+    int peek();
+}
+//SAME CODE
